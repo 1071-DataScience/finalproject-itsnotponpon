1ef177b9ebbba2952676f0600bde788618f0b928
itsnotponpon <johnpon0712@gmail.com>
2019-01-13 12:37:07 +0800

    Model tweak and parser take 3

--- code/model.R
+++ code/model.R
@@ -12,75 +12,92 @@ pred_f <- argv$pred
 if (pred_f == '') {
   pred_f <- sub('test', 'pred', test_f)
 }
-cat(train_f, test_f, pred_f)
-# Somehow get train and test file names...
-train_f <- '../data_fr_en/fr_en.slam.20171218.train'
-test_f <- '../data_fr_en/fr_en.slam.20171218.test'
 
 # The dataset format is almost SSV, but not quite
 load_data <- function(filename, is_train) {
   raw_lines <- readLines(filename)
   # Read the SSV part first
-  df <- read.table(filename, header=F, as.is=c(1, 4))
+  # `uid`, `token`, and `morph` is kept as string for preprocessing
+  df_part <- read.table(filename, header=F, as.is=c(1, 2, 4))
   if (is_train) {
-    colnames(df) <- c('uid', 'token', 'pos', 'morph', 'depLabel', 'depHead', 'label')
+    colnames(df_part) <- c('uid', 'token', 'pos', 'morph', 'depLabel', 'depHead', 'label')
   } else {
-    colnames(df) <- c('uid', 'token', 'pos', 'morph', 'depLabel', 'depHead')
+    colnames(df_part) <- c('uid', 'token', 'pos', 'morph', 'depLabel', 'depHead')
   }
   # Filter out the exercise header
   wh <- which(startsWith(raw_lines, '#'))
   # Count where they originally belong
-  exercise_len <- c()
+  exercise_len <- vector(mode='integer', length=length(wh))
   for (i in 1:length(wh)-1) {
-    exercise_len <- c(exercise_len, wh[i+1] - wh[i])
+    exercise_len[i] <- wh[i+1] - wh[i]
   }
-  exercise_len <- sapply(exercise_len, function(x) {x - 2})
-  exercise_len <- c(exercise_len, nrow(df) - sum(exercise_len))
+  exercise_len <- exercise_len - 2
+  exercise_len[i+1] <- nrow(df_part) - sum(exercise_len) - 2
+  # Read and replicate headers
   headers <- raw_lines[wh]
   l <- strsplit(headers, '\\s+|:')
   l <- rep.int(l, exercise_len)
   # Fill in various variables, then return it
-  df$user <- sapply(l, function(x) {x[[3]]}, simplify=T)
-  df$countries <- sapply(l, function(x) {x[[5]]}, simplify=T)
-  df$days <- as.numeric(sapply(l, function(x) {x[[7]]}, simplify=T))
-  df$client <- as.factor(sapply(l, function(x) {x[[9]]}, simplify=T))
-  df$session <- as.factor(sapply(l, function(x) {x[[11]]}, simplify=T))
-  df$format <- as.factor(sapply(l, function(x) {x[[13]]}, simplify=T))
-  df$time <- as.numeric(sapply(l, function(x) {x[[15]]}, simplify=T))
-  # Extra preprocessing
-  df$time[is.na(df$time)] <- 0
-  df$time[df$time < 0] <- 0
-  df
+  # l[[1]] is '#', even numbers are feature names
+  ex_user      <- sapply(l, function(x) {x[[3]]}, simplify=T)
+  ex_countries <- sapply(l, function(x) {x[[5]]}, simplify=T)
+  ex_days      <- as.numeric(sapply(l, function(x) {x[[7]]}, simplify=T))
+  ex_client    <- as.factor(sapply(l, function(x) {x[[9]]}, simplify=T))
+  ex_session   <- as.factor(sapply(l, function(x) {x[[11]]}, simplify=T))
+  ex_format    <- as.factor(sapply(l, function(x) {x[[13]]}, simplify=T))
+  ex_time      <- as.numeric(sapply(l, function(x) {x[[15]]}, simplify=T))
+  exercises <- data.frame(user=ex_user,
+                          countries=ex_countries,
+                          days=ex_days,
+                          client=ex_client,
+                          session=ex_session,
+                          format=ex_format,
+                          time=ex_time)
+
+  cbind(exercises, df_part)
 }
-# test_d <- load_data(test_f, F)
+# After another round of optimization we can read original files with acceptable speed
+train_d <- load_data(train_f, is_train=T)
+test_d <- load_data(test_f, is_train=F)
 
-train_d <- read.csv('data/train_d.csv', as.is=c(1, 4, 9))
-test_d <- read.csv('data/test_d.csv', as.is=c(1, 4, 8))
-# write.csv(train_d, 'train_d.csv', row.names=F)
-# write.csv(test_d, 'test_d.csv', row.names=F)
-
-# 'Fixing' features
+# 'Fixing' features, including:
+# * filter out less than 1 day of usage
+# * Lowercase tokens
+# * Split exercise and token indices
+# * Replacing negative and `NA` time with zero (not the best, better guess them)
 train_d <- train_d[train_d$days > 1,]
+train_d$token <- factor(tolower(train_d$token))
+train_d$exerciseId <- factor(sapply(train_d$uid, function(x) {substr(x, 1, 8)}))
+train_d$exerciseIndex <- as.numeric(sapply(train_d$uid, function(x) {substr(x, 9, 10)}))
+train_d$tokenIndex <- as.numeric(sapply(train_d$uid, function(x) {substr(x, 11, 12)}))
 train_d$time[train_d$time < 0] <- 0
-test_d$time[test_d$time < 0] <- 0
 train_d$time[is.na(train_d$time)] <- 0
-test_d$time[is.na(test_d$time)] <- 0
-train_d$countries <- factor(sapply(strsplit(train_d$countries, split='|', fixed=T), function(x) {x[1]}, simplify=T))
-test_d$countries <- factor(sapply(strsplit(test_d$countries, split='|', fixed=T), function(x) {x[1]}, simplify=T))
+# train_d$countries <- factor(sapply(strsplit(train_d$countries, split='|', fixed=T), function(x) {x[1]}, simplify=T))
 
+test_d$token <- factor(tolower(test_d$token))
+test_d$exerciseId <- factor(sapply(test_d$uid, function(x) {substr(x, 1, 8)}))
+test_d$exerciseIndex <- as.numeric(sapply(test_d$uid, function(x) {substr(x, 9, 10)}))
+test_d$tokenIndex <- as.numeric(sapply(test_d$uid, function(x) {substr(x, 11, 12)}))
+test_d$time[test_d$time < 0] <- 0
+test_d$time[is.na(test_d$time)] <- 0
+# test_d$countries <- factor(sapply(strsplit(test_d$countries, split='|', fixed=T), function(x) {x[1]}, simplify=T))
+
+# rpart model (currently unused)
 # rp_control <- rpart.control(minsplit=10L, xval=10)
-glm_model <- glm(label ~ pos + depHead + depLabel + days +
-                 countries + session + format + time,
-                data=train_d,
-                family=binomial())
 # rp_model <- rpart(label ~ pos + depHead + depLabel + days +
 #                   countries + session + format + time,
 #                   data=train_d,
 #                   control=rp_control)
 
-print(summary(glm_model))
+glm_model <- glm(label ~ pos + depHead + depLabel + days +
+                 session + format + time +
+                tokenIndex + exerciseIndex,
+                data=train_d,
+                family=binomial())
+
+# print(summary(glm_model))
 
 prediction <- predict(glm_model, test_d, type='response')
 # prediction[is.na(prediction)] <- 0.0001
 out_d <- data.frame(uid=test_d$uid, pred=prediction)
-write.table(out_d, 'pred.csv', quote=F, col.names=F, row.names=F)
+write.table(out_d, pred_f, quote=F, col.names=F, row.names=F)
